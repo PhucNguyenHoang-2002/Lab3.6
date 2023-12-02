8ca214091b87ba44e05b7ac615c5e84dd31d544a
PhucNguyenHoang-2002 <116200803+PhucNguyenHoang-2002@users.noreply.github.com>
2023-12-02 13:35:35 +0700

    Update ContactComponent.js

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
+import * as MailComposer from 'expo-mail-composer';
 
 import * as Animatable from 'react-native-animatable';
 
@@ -16,10 +17,20 @@ class Contact extends Component {
           <Text style={{ margin: 10 }}>HONG KONG</Text>
           <Text style={{ margin: 10 }}>Tel: +852 1234 5678</Text>
           <Text style={{ margin: 10 }}>Fax: +852 8765 4321</Text>
-          <Text style={{ margin: 10 }}>Email: confusion@food.net</Text>
+          <Text style={{ margin: 10 }}>Email: phuc.nh12490@sinhvien.hoasen.edu.vn</Text>
+          <Button title=' Compose Email' buttonStyle={{ backgroundColor: '#7cc' }}
+            icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+            onPress={this.composeMail} />
         </Card>
       </Animatable.View>
     );
+  }
+  composeMail() {
+    MailComposer.composeAsync({
+      recipients: ['nguyenhoangphuc2002vn@gmail.com'],
+      subject: 'From PhucNH',
+      body: 'Hello my friends ...'
+    });
   }
 }
 export default Contact;
